1da55d879c7f62da33a09162192f4ec06b129c65
arponp <arponp30@gmail.com>
2023-12-31 13:20:02 -0800

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Arpon Purkayastha on 12/31/23.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
