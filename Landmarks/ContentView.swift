13162d5be036795a814a3335300ceb72d049ad04
arponp <arponp30@gmail.com>
2023-12-31 13:55:05 -0800

    add MapView

--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -9,8 +9,18 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        VStack(alignment: .leading) {
             Text("Turtle Rock")
-            .font(.title)
+                .font(.title)
+            HStack {
+                Text("Joshua Tree National Park")
+                    .font(.subheadline)
+                Spacer()
+                Text("California")
+                    .font(.subheadline)
+            }
+        }
+        .padding()
             
     }
 }
